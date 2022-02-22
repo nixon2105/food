06f7b83316650ded31a3f0ec3861de8559887f34
nixon2105 <radivilov1991@mail.ru>
2022-02-22 18:27:57 +0300

    feat: add functions hideTabContent and showTabContent

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const tabs = document.querySelectorAll('.tabheader__item');
+  const tabsContent = document.querySelectorAll('.tabcontent');
+  const tabsParent = document.querySelector('.tabheader__item');
 
+  const hideTabContent = () => {
+    tabsContent.forEach((item) => {
+      item.style.display = 'none';
+    });
+
+    tabs.forEach((item) => {
+      item.classList.remove('.tabheader__item_active');
+    });
+  };
+
+  const showTabContent = (i = 0) => {
+    tabsContent[i].style.display = 'block';
+    item.classList.add('.tabheader__item_active');
+  };
+
+  hideTabContent();
+  showTabContent();
+});
