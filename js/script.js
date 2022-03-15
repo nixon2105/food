b04d3354b25c64cfd3b1ca236d220c939c3e9996
nixon2105 <radivilov1991@mail.ru>
2022-03-15 12:58:55 +0300

    refactor: function open

--- js/script.js
+++ js/script.js
@@ -95,19 +95,21 @@ window.addEventListener('DOMContentLoaded', () => {
     modal = document.querySelector('.modal'),
     modalCloseBtn = document.querySelector('[data-close]');
 
+  function openModal() {
+    modal.classList.add('show');
+    modal.classList.remove('hide');
+    document.body.style.overflow = 'hidden';
+    clearInterval(modalTimerId);
+  }
+
   modalTrigger.forEach((btn) => {
-    btn.addEventListener('click', function () {
-      modal.classList.add('show');
-      modal.classList.remove('hide');
-      // Либо вариант с toggle - но тогда назначить класс в верстке
-      document.body.style.overflow = 'hidden';
-    });
+    btn.addEventListener('click', openModal);
   });
 
   function closeModal() {
     modal.classList.add('hide');
     modal.classList.remove('show');
-    // Либо вариант с toggle - но тогда назначить класс в верстке
+
     document.body.style.overflow = '';
   }
 
@@ -124,4 +126,6 @@ window.addEventListener('DOMContentLoaded', () => {
       closeModal();
     }
   });
+
+  const modalTimerId = setTimeout(openModal, 5000);
 });
