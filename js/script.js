a5d391a7cd00f72fd478e6eff2e748fb7988e1a1
nixon2105 <radivilov1991@mail.ru>
2022-03-19 18:04:46 +0300

    feat: add function postData

--- js/script.js
+++ js/script.js
@@ -213,4 +213,46 @@ window.addEventListener('DOMContentLoaded', () => {
     '.menu .container',
     'menu__item'
   ).render();
+
+  //FORMS
+
+  const form = document.querySelectorAll('form');
+
+  const message = {
+    loading: 'Загрузка',
+    success: 'Спасибо! Скоро мы с вами свяжемся',
+    failure: 'Что-то пошло не так...',
+  };
+
+  form.forEach((item) => {
+    postData(item);
+  });
+
+  function postData(form) {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      const statusMessage = document.createElement('div');
+      statusMessage.classList.add('status');
+      statusMessage.textContent = message.loading;
+      form.append(statusMessage);
+
+      const request = new XMLHttpRequest();
+      request.open('POST', 'server.php');
+
+      request.setRequestHeader('Content-type', 'multipart/form-data');
+      const formData = new FormData(form);
+
+      request.send(formData);
+
+      request.addEventListener('load', () => {
+        if (request.status === 200) {
+          console.log(request.response);
+          statusMessage.textContent = message.success;
+        } else {
+          statusMessage.textContent = message.failure;
+        }
+      });
+    });
+  }
 });
