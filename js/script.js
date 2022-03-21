22cc26d42667496cb39b458a97175cdecb40edba
nixon2105 <radivilov1991@mail.ru>
2022-03-21 14:57:15 +0300

    refactor: script.js

--- js/script.js
+++ js/script.js
@@ -305,8 +305,4 @@ window.addEventListener('DOMContentLoaded', () => {
       closeModal();
     }, 4000);
   }
-
-  fetch('http://localhost:3000/menu')
-    .then((data) => data.json())
-    .then((res) => console.log(res));
 });
