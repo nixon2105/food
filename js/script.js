b0d3aea2a751ece99ad18ea9db58fbd6fa33002d
nixon2105 <radivilov1991@mail.ru>
2022-03-22 09:48:34 +0300

    feat: add change slider counter

--- js/script.js
+++ js/script.js
@@ -313,8 +313,18 @@ window.addEventListener('DOMContentLoaded', () => {
 
   const prev = document.querySelector('.offer__slider-prev');
   const next = document.querySelector('.offer__slider-next');
+  const total = document.querySelector('#total');
+  const current = document.querySelector('#current');
 
   let slideIndex = 1;
+
+  showSlides(slideIndex);
+
+  if (slides.length < 10) {
+    total.textContent = `0${slides.length}`;
+  } else {
+    total.textContent = slides.length;
+  }
 
   function showSlides(n) {
     if (n > slides.length) {
@@ -327,9 +337,23 @@ window.addEventListener('DOMContentLoaded', () => {
     slides.forEach((item) => (item.style.display = 'none'));
 
     slides[slideIndex - 1].style.display = 'block';
+
+    if (slides.length < 10) {
+      current.textContent = `0${slideIndex}`;
+    } else {
+      current.textContent = slideIndex;
+    }
   }
 
   function plusSlides(n) {
     showSlides((slideIndex += n));
   }
+
+  prev.addEventListener('click', () => {
+    plusSlides(-1);
+  });
+
+  next.addEventListener('click', () => {
+    plusSlides(+1);
+  });
 });
