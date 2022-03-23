bb54bc0e91da26d69b32f95e31db56edef79af33
nixon2105 <radivilov1991@mail.ru>
2022-03-23 08:21:25 +0300

    feat: add function calcTotal

--- js/script.js
+++ js/script.js
@@ -521,4 +521,26 @@ window.addEventListener('DOMContentLoaded', () => {
   // next.addEventListener('click', () => {
   //   plusSlides(+1);
   // });
+
+  //-----------------Calculator----------------//
+
+  const result = document.querySelector('.calculating__result span');
+  let sex, height, weight, age, ratio;
+
+  function calcTotal() {
+    if (!sex || !height || !weight || !age || !ratio) {
+      result.textContent = '____';
+      return;
+    }
+
+    if (sex === 'female') {
+      result.textContent =
+        (447.6 + 9.2 * weight + 3.1 * height - 4.3 * age) * ratio;
+    } else {
+      result.textContent =
+        (88.36 + 13.4 * weight + 4.8 * height - 5.7 * age) * ratio;
+    }
+  }
+
+  calcTotal();
 });
