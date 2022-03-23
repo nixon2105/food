0a4e1806e497822d99ac1dc47a7f86116388291c
nixon2105 <radivilov1991@mail.ru>
2022-03-23 08:40:11 +0300

    fix: index.html

--- js/script.js
+++ js/script.js
@@ -525,22 +525,29 @@ window.addEventListener('DOMContentLoaded', () => {
   //-----------------Calculator----------------//
 
   const result = document.querySelector('.calculating__result span');
-  let sex, height, weight, age, ratio;
+  let sex = 'female',
+    height,
+    weight,
+    age,
+    ratio = 1.375;
 
   function calcTotal() {
     if (!sex || !height || !weight || !age || !ratio) {
       result.textContent = '____';
       return;
     }
-
     if (sex === 'female') {
-      result.textContent =
-        (447.6 + 9.2 * weight + 3.1 * height - 4.3 * age) * ratio;
+      result.textContent = Math.round(
+        (447.6 + 9.2 * weight + 3.1 * height - 4.3 * age) * ratio
+      );
     } else {
-      result.textContent =
-        (88.36 + 13.4 * weight + 4.8 * height - 5.7 * age) * ratio;
+      result.textContent = Math.round(
+        (88.36 + 13.4 * weight + 4.8 * height - 5.7 * age) * ratio
+      );
     }
   }
+
+  calcTotal();
 
   calcTotal();
 
