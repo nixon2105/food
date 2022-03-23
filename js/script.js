aba3ba031040e1bf777acec1c87ab0ec7f2c51c5
nixon2105 <radivilov1991@mail.ru>
2022-03-23 09:27:15 +0300

    feat: add check input value

--- js/script.js
+++ js/script.js
@@ -581,6 +581,12 @@ window.addEventListener('DOMContentLoaded', () => {
     const input = document.querySelector(selector);
 
     input.addEventListener('input', () => {
+      if (input.value.match(/\D/g)) {
+        input.style.border = '1px solid red';
+      } else {
+        input.style.border = 'none';
+      }
+
       switch (input.getAttribute('id')) {
         case 'height':
           height = +input.value;
